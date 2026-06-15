034d35d7e021b427cb58d2c4d6b62139ae13b990
@dissoares <diegosoaresyt@gmail.com>
2026-06-15 11:31:55 -0300

    adicionando analytcs

--- public/install.php
+++ public/install.php
@@ -34,6 +34,18 @@ try {
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-405Z6HMN1Z"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-405Z6HMN1Z');
+</script>
+
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Instalação BetCopa</title>
